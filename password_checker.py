6b96f1a4eda12abacc9df7e1a8ac610614808223
Nicolle <nicollesosa.n@gmail.com>
2026-09-07 15:06:32 -0400

    Fixing logic and adding screenshots.

--- password_checker.py
+++ password_checker.py
@@ -14,15 +14,15 @@ special_characters = r"[!@#$%&_\-*+]"
 if len(create_password) >= min_length:
 # Temporary result variables used to verify each length check during testing
     length_check = passed
-    print("Length Check Satisfied")
+    print("Length Check Reached")
 else:
     length_check = failed
-    print("Length Check Not Satisfied")
+    print("Length Check Not Reached")
 
 #Uppercase Condition
-uppercase_check = re.search(r"[A-Z]", create_password)
+upcase_result = re.search(r"[A-Z]", create_password)
 
-if uppercase_check:
+if upcase_result:
     # Temporary result variables used to verify each Upper check during testing
     upcase_result = passed
     print("Atleast one Character is Uppercase!")
@@ -31,9 +31,9 @@ else:
     print("No Characters Are Uppercase")
 
 #Lowercase Condition
-lowercase_check = re.search(r"[a-z]", create_password)
+lowcase_result = re.search(r"[a-z]", create_password)
 
-if lowercase_check:
+if lowcase_result:
     # Temporary result variables used to verify each Lowercase check during testing
     lowcase_result = passed
     print("Atleast one character is Lowercase!")
@@ -42,9 +42,9 @@ else:
     print("No Characters Are Lowercase")
 
 #Numbercase Condition
-numbercase_check = re.search(r"[0-9]", create_password)
+numcase_result = re.search(r"[0-9]", create_password)
 
-if numbercase_check:
+if numcase_result:
     # Temporary result variables used to verify each Numbercase check during testing
     numcase_result = passed
     print("Atleast one character is a number!")
@@ -53,9 +53,9 @@ else:
     print("No Number Characters Included")
 
 #Special Characters Condition
-search_char = re.search(special_characters, create_password)
+special_result = re.search(special_characters, create_password)
 
-if search_char:
+if special_result:
     # Temporary result variables used to verify special character check during testing
     special_result = passed
     print("Atleast one special character included")
@@ -65,11 +65,13 @@ else:
 
 #Strength Checker - Weak, Medium, or Strong
 
-if len(create_password) >= min_length and uppercase_check and lowercase_check and numbercase_check and search_char:
+character_score = sum([upcase_result, lowcase_result, numcase_result, special_result])
+
+if len(create_password) >= min_length and character_score == 4:
     strength = "Strong"
-elif len(create_password) >= min_length and uppercase_check and lowercase_check and numbercase_check and not search_char:
+elif len(create_password) >= min_length and character_score == 3:
     strength = "Medium"
 else:
     strength = "Weak"
-
+print(character_score)
     
